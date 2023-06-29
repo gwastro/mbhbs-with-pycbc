67ae27566bcd48646ade121503f4c3c627bae31f
Connor Weaving <connor@login7.pri.sciama3.alces.network>
2023-06-29 13:54:10 +0100

    Inital file structure testing

---
+++ inference/no_gbs/single/sig_gen_nogb.py
@@ -0,0 +1,154 @@
+from pycbc.waveform import get_fd_det_waveform_sequence
+from pycbc.types import TimeSeries
+from pycbc.types import zeros
+from pycbc.frame import write_frame
+from pycbc.psd import interpolate
+
+from ldc.common.series import TDI
+import ldc.io.hdf5 as hdfio
+from ldc.common import tools
+
+import numpy as np
+import pandas as pd
+
+
+
+sangria_training = "/users/connor/main/datasets/training/LDC2_sangria_training_v2.h5"
+
+# Reading in noiseless data with LDC code
+tdi_nn = hdfio.load_array(sangria_training, name="sky/mbhb/tdi")
+
+tdi_nn = TDI.load(sangria_training, name="sky/mbhb/tdi")
+
+X_nn = tdi_nn['X']
+Y_nn = tdi_nn['Y']
+Z_nn = tdi_nn['Z']
+
+A_nn = (Z_nn - X_nn)/np.sqrt(2)
+E_nn = (X_nn - 2*Y_nn + Z_nn)/np.sqrt(6)
+T_nn = (X_nn + Y_nn + Z_nn)/np.sqrt(3)
+
+
+tmin=0.
+tmax=31536000.
+dt=5.
+t_range = np.arange(tmin,tmax,dt)
+
+# Reading in standard noisy data
+tdi_ts, tdi_descr = hdfio.load_array(sangria_training, name="obs/tdi")
+
+X = tdi_ts['X']
+Y = tdi_ts['Y']
+Z = tdi_ts['Z']
+
+A = (Z - X)/np.sqrt(2)
+E = (X - 2*Y + Z)/np.sqrt(6)
+T = (X + Y + Z)/np.sqrt(3)
+
+
+# Noise including GBs
+noise_A_withgb = TimeSeries(A - A_nn, delta_t=5.)
+noise_E_withgb = TimeSeries(E - E_nn, delta_t=5.)
+noise_T_withgb = TimeSeries(T - T_nn, delta_t=5.)
+
+
+# Generating GBs to be removed from igb, dgb, vgb files.
+for i in ['v','d','i']:
+
+    print(f"{i}gb")
+
+    tdi_gbs = TDI.load(sangria_training, name=f"sky/{i}gb/tdi")
+
+    gb_X = tdi_gbs['X']
+    gb_Y = tdi_gbs['Y']
+    gb_Z = tdi_gbs['Z']
+
+    gb_A = (gb_Z - gb_X)/np.sqrt(2)
+    gb_E = (gb_X - 2*gb_Y + gb_Z)/np.sqrt(6)
+    gb_T = (gb_X + gb_Y + gb_Z)/np.sqrt(3)
+
+    noise_A_withgb -= TimeSeries(gb_A, delta_t=5.)
+    noise_E_withgb -= TimeSeries(gb_E, delta_t=5.)
+    noise_T_withgb -= TimeSeries(gb_T, delta_t=5.)
+
+
+# Noise without gbs
+noise_A = noise_A_withgb
+noise_E = noise_E_withgb
+noise_T = noise_T_withgb
+
+
+
+
+# Reading in MBHB signals from Sangria blind
+sangria_fn = "/users/connor/main/datasets/mbhb-unblinded.h5"
+mbhb, units = hdfio.load_array(sangria_fn, name="sky/mbhb/cat")
+pd.DataFrame(mbhb)
+sigs = pd.DataFrame(mbhb).to_dict('records')
+
+
+def spin_conv(mag, pol):
+    return mag*np.cos(pol)
+
+bbhx_sigs = {}
+
+for i in range(6):
+    wave = sigs[i]
+
+    psi, incl = tools.aziPolAngleL2PsiIncl(wave["EclipticLatitude"],
+                                       wave["EclipticLongitude"],
+                                       wave['InitialPolarAngleL'],
+                                       wave['InitialAzimuthalAngleL'])
+
+    wave['approximant'] = 'BBHX_PhenomD'
+    wave['mass1'] = wave.pop('Mass1')
+    wave['mass2'] = wave.pop('Mass2')
+    wave['spin1z'] = spin_conv(wave['Spin1'],wave['PolarAngleOfSpin1'])
+    wave['spin2z'] = spin_conv(wave['Spin2'],wave['PolarAngleOfSpin2'])
+    wave['tc'] = wave.pop('CoalescenceTime')
+    wave['distance'] = wave.pop('Distance')
+    wave['inclination'] = incl
+    wave['polarization'] = psi
+    wave['eclipticlatitude'] = wave.pop('EclipticLatitude')
+    wave['eclipticlongitude'] = wave.pop('EclipticLongitude')
+    wave['coa_phase'] = wave.pop('PhaseAtCoalescence')
+    wave['f_final'] = 0.1
+    wave['t_obs_start'] = 31536000.
+    del wave['PolarAngleOfSpin1'], wave['PolarAngleOfSpin2'], wave['Spin1'],\
+wave['Spin2'], wave['InitialPolarAngleL'], wave['Redshift'],\
+wave['ObservationDuration'], wave['InitialAzimuthalAngleL'], wave['Cadence']
+    bbhx_sigs[f'mbhb{i}'] = wave
+
+
+
+psd_time = 6307200
+sample_length = 31
+
+# Injecting BBHx signal with noise from training without GBs. Also saves a psd
+# for each gwf (not one psd for all 6 sigs in one gwf).
+for i in range(6):
+    params = bbhx_sigs[f'mbhb{i}']
+    tdi = get_fd_det_waveform_sequence(ifos=['LISA_A','LISA_E','LISA_T'], **params, ref_frame='SSB')
+    A_b = tdi['LISA_A'].to_timeseries()
+    E_b = tdi['LISA_E'].to_timeseries()
+    T_b = tdi['LISA_T'].to_timeseries()
+
+    A_data = TimeSeries(zeros(31536000/5,dtype=float),delta_t=5,epoch=0) + noise_A + A_b
+    E_data = TimeSeries(zeros(31536000/5,dtype=float),delta_t=5,epoch=0) + noise_E + E_b
+    T_data = TimeSeries(zeros(31536000/5,dtype=float),delta_t=5,epoch=0) + noise_T + T_b
+
+    Apsd = A_data.psd(psd_time/sample_length)
+    Epsd = E_data.psd(psd_time/sample_length)
+    Tpsd = T_data.psd(psd_time/sample_length)
+    Apsd = interpolate(Apsd, A_data.delta_f)
+    Epsd = interpolate(Epsd, E_data.delta_f)
+    Tpsd = interpolate(Tpsd, T_data.delta_f)
+    Apsd.save(f'files/A_psd_{i}.txt')
+    Epsd.save(f'files/E_psd_{i}.txt')
+    Tpsd.save(f'files/T_psd_{i}.txt')
+
+    write_frame(f'files/{i}_A_nogb.gwf', 'LA:LA', A_data)
+    write_frame(f'files/{i}_E_nogb.gwf', 'LE:LE', E_data)
+    write_frame(f'files/{i}_T_nogb.gwf', 'LT:LT', T_data)
+
+    print(f'Signal {i} generated')
